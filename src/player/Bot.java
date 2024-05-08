53c8bd19290004abc1076dfe55bdc5b8ea38867b
Simon Husser <simonhusser@unistra.fr>
2024-05-08 18:53:44 +0200

    ajout des classes enfants de Player

---
+++ src/player/Bot.java
@@ -0,0 +1,64 @@
+package player;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import pokemoon.Pokemoon;
+
+public class Bot extends Player {
+    public Bot(String nom, boolean firstPlayer) {
+        super(nom, firstPlayer);
+    }
+
+    public boolean botAttack(Player other, int myPokeIndex) {
+        Pokemoon myPoke = m_playground.getPokemoonByIndex(myPokeIndex);
+        ArrayList<Pokemoon> otherAffin = new ArrayList<Pokemoon>();
+        String element = myPoke.getElement();
+        for(int i=0; i<3; i++){
+            if(other.m_playground.getPokemoonByIndex(i).getDesavantage().equals(element)){
+                otherAffin.add(other.m_playground.getPokemoonByIndex(i));
+            }
+        }
+        if(!otherAffin.isEmpty()) {
+            Pokemoon target = otherAffin.get(0);
+            for (Pokemoon p : otherAffin) {
+                if (p.getLife() < target.getLife()) {
+                    target = p;
+                }
+            }
+            myPoke.fight(target);
+            return true;
+        }
+        else {
+            ArrayList<Pokemoon> alivePokemons = new ArrayList<Pokemoon>();
+            for(int i=0; i<3; i++){
+                alivePokemons.add(other.m_playground.getPokemoonByIndex(i));
+            }
+            if (!alivePokemons.isEmpty()) {
+                Pokemoon target;
+                int minLife = alivePokemons.get(0).getLife();
+                ArrayList<Pokemoon> candidates = new ArrayList<>();
+            
+                for (Pokemoon p : alivePokemons) {
+                    if (p.getLife() < minLife) {
+                        minLife = p.getLife();
+                        candidates.clear();
+                        candidates.add(p);
+                    } 
+                    else if (p.getLife() == minLife) {
+                        candidates.add(p);
+                    }
+                }
+            
+                Random random = new Random();
+                target = candidates.get(random.nextInt(candidates.size()));
+            
+                myPoke.fight(target);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/player/User.java
@@ -0,0 +1,27 @@
+package player;
+import pokemoon.*;
+
+public class User extends Player {
+    public User(String nom, boolean firstPlayer) {
+        super(nom, firstPlayer);
+    }
+
+    /**
+     * permet d'attaquer un pokemon d'un autre joueur (si on veut jouer a plus de deux)
+     * @param other le joueur que l'on veut embête
+     * @param namePoke le nom de poke que l'on veut éliminer
+     * @return Retourne true si le pokemon a été attaque, false sinon
+     * /!\ le champ other ne peut pas être null
+     * /!\ le champ namePoke vide veut dire que l'on attaque un pokemon sur lequel on a un avantage
+     */
+    public boolean userAttack(Player other, int myPokeIndex, int otherPokeIndex) {
+        //trouve le poke que je veux utiliser pour attaquer et test si null
+        Pokemoon myPoke = m_playground.getPokemoonByIndex(myPokeIndex);
+        // trouve le pokemon que je veux attaquer et test si null
+        Pokemoon otherPoke = other.m_playground.getPokemoonByIndex(otherPokeIndex);
+
+        
+        myPoke.fight(otherPoke);
+        return true;
+    }
+}
